0e4c402faf059d948c2e6a4ca5b54b50ffc37ea3
aborisova <aborisova@gan.com>
2024-04-13 15:52:33 +0300

    new push

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -13,3 +13,4 @@ test('Check about page test', async ({ page }) => {
   const text = await heading.textContent();
   expect(text).toBe('This is the Regular exam for Software Engineering and DevOps course @ SoftUni');
 });
+
